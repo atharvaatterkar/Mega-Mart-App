0ab7d06dab00b716636e48d8878232f285acbb2a
Atharva Atterkar <136713071+atharvaatterkar@users.noreply.github.com>
2024-12-07 16:35:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,44 @@
 import express from "express";
 import colors from "colors";
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import authRoutes from './routes/authRoute.js'
-//congif env
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
+
+//configure env
 dotenv.config();
+
+//databse config
+connectDB();
 
 //rest object
 const app = express();
 
-//middelware
+//middelwares
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
-app.use("/api/vi/auth", authRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
-//rest api 
-app.get('/', (req, res) => {
-    res.send("<h1>welcome to MegaMart</h1>");
+//rest api
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to MegaMart app</h1>");
 });
 
-
-
-//port
-const PORT = process.env.PORT || 8080;
+//PORT
+const PORT = process.env.PORT || 8081;
 
 //run listen
 app.listen(PORT, () => {
-    console.log('Server runnig on${process.env.DEV_MODE} mode on port ${PORT}'.bgCyan.white);
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
 });
